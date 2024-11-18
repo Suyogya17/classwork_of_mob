ef8e50263814e80f651547c29d3be6a554bdc3b8
Suyogya17 <suyogyashrestha17@gmail.com>
2024-11-18 10:30:57 +0545

    checking character

---
+++ checkingcharecter.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main() {
+  print("Enter a single character:");
+
+  String? input = stdin.readLineSync();
+
+  if (input != null && input.length == 1) {
+    String char = input.toLowerCase();
+
+    if ("aeiou".contains(char)) {
+      print("$char is a vowel.");
+    } else if (RegExp(r'^[a-z]$').hasMatch(char)) {
+      print("$char is a consonant.");
+    } else {
+      print("$char is not a valid alphabet character.");
+    }
+  } else {
+    print("Please enter a single alphabet character.");
+  }
+}
