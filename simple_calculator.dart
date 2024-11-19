cd1d01297bbdc7ebcbda691d92a2fa07daadf0a2
Suyogya17 <suyogyashrestha17@gmail.com>
2024-11-19 21:07:21 +0545

    simple calculator

---
+++ simple_calculator.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+void main() {
+  print("Enter the first number:");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the second number:");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  print("Choose an operation (+, -, *, /):");
+  String? operation = stdin.readLineSync();
+
+  double? result;
+
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      if (num2 != 0) {
+        result = num1 / num2;
+      } else {
+        print("Division by zero is not allowed.");
+        return;
+      }
+      break;
+    default:
+      print("Invalid operation.");
+      return;
+  }
+
+  print("The result of $num1 $operation $num2 is $result.");
+}
