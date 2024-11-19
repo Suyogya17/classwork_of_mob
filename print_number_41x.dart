e05b6aa4d71900bebdfc52308bce54c2c333603a
Suyogya17 <suyogyashrestha17@gmail.com>
2024-11-19 21:09:35 +0545

    printing number except 41

---
+++ print_number_41x.dart
@@ -0,0 +1,12 @@
+void printNumbersExcept41() {
+  for (int i = 1; i <= 100; i++) {
+    if (i == 41) {
+      continue; // Skip 41
+    }
+    print(i);
+  }
+}
+
+void main() {
+  printNumbersExcept41();
+}
